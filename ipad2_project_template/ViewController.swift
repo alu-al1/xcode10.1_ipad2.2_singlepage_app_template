1688d206a93496c865cf5b98a76e65e3c34d792c
alu_al1 <andrei.povarov+apple@proton.me>
2025-01-03 03:16:49 +0300

    initial commit (compiles but cannot run on ipad2 yet)

---
+++ ipad2_project_template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ipad2_project_template
+//
+//  Created by alu_al1 on 3.01.2025.
+//  Copyright © 2025 alu_al1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
